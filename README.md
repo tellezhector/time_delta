bbaa6eb950afd68bbdb9096938dbad91c29ce81b
Hector Tellez <tellezhector@users.noreply.github.com>
2024-09-05 02:28:48 -0700

    Adds option to print commented delta. Adds option to print help.

--- README.md
+++ README.md
@@ -1,30 +1,46 @@
 # time_delta.py
 
-Self link: https://github.com/tellezhector/time_delta
+Utility script to print two (recent) timestamps with the given delta.
 
-Utility script to print two (recent) timestamps in iso-format that have the given delta.
-The fist timestamp is always the in the past in respect to the second (or be the same).
+Timestamps are printed in ISO format.
+
+Source: https://github.com/tellezhector/time_delta
 
 ## Usage:
 
 ```txt
-./time_delta.py delta [shift]
+Usage:  ./time_delta.py [-h] [-n] [-v] delta [shift]
+Output: <before> - <after>  [# delta]
 
-`delta` can be of the formats:
-        1:00:00:00, 1:00:03:00,   1:00:03:00, 3:00:00, 1:00:00, 3:02:50 or equivalently
-        1day,       1day3minutes, 1d3m,       3h,      1h,      3h2m50s 
+The fist timestamp is always the in the past in respect to the second (or be 
+the same).
 
-`shift` can be of any of the same kind of formats.
-        `shift` can be preceeded by a minus sign (-).
+ delta  can be of the formats: 
+        1day,       1day3minutes,       1d3m,      3h,      1h, 3h2m50s 
+        (or equivalently)
+        1:00:00:00,   1:00:03:00, 1:00:03:00, 3:00:00, 1:00:00, 3:02:50
 
-   `-n` do not print a new line at the end.
+ shift  optional. Can be of any of the same kind of formats as `delta`. 
+        If undefined, <after> will be 'now'; if defined, the dates will be 
+        shifted by the time specified, `shift` can be preceeded by a
+        minus (-) or plus (+) sign, a minus sign shifts "to the past", a plus
+        sign shifts "to the future", plus sign is the same as no sign.
 
-Note: `delta` can also be negative, in which case the resulting delta will be a time
-addition instead of a substraction, but this doesn't seem very intuitive nor useful.
+    -n  do not print a new line at the end.
+
+    -v  print the original delta after the timestamps in format:
+        <before> - <after>  #  delta
+        this is verifying the delta length easier visually.
+
+    -h  prints help, -hh, -hhh and -hhhh will print more details increasingly.
+
+Note: `delta` can also be negative, in which case the resulting delta will be 
+a time addition instead of a substraction, but this doesn't seem very 
+intuitive nor useful.
 ```
 
 
-## Examples:
+## Examples
 
 Assuming the current time is 2024-09-04 02:59:53-07:00
 
@@ -47,6 +63,13 @@ Shifting 3 seconds into the past:
 ```shell
 $ ./time_delta.py 1d -3s
 2024-09-03 02:59:50-07:00 - 2024-09-04 02:59:50-07:00
+```
+
+The same but including the delta:
+
+```shell
+$ ./time_delta.py -v 1d -3s
+2024-09-03 02:59:50-07:00 - 2024-09-04 02:59:50-07:00  # 1d
 ```
 
 A delta between now and a day from now:
--- time_delta.py
+++ time_delta.py
@@ -1,67 +1,97 @@
 #!/usr/bin/env python3
-#
-# https://github.com/tellezhector/time_delta
-#
-# Utility script to print two (recent) timestamps in iso-format that have the given delta.
-# The fist timestamp is always the in the past in respect to the second (or be the same).
-#
-# Usage:
-#
-# ./time_delta.py [-n] delta [shift]
-#
-# `delta` can be of the formats: 
-#         1day,       1day3minutes,       1d3m,      3h,      1h, 3h2m50s or equivalently
-#         1:00:00:00,   1:00:03:00, 1:00:03:00, 3:00:00, 1:00:00, 3:02:50
-#
-# `shift` can be of any of the same kind of formats.
-#         `shift` can be preceeded by a minus sign (-).
-#
-#    `-n` do not print a new line at the end.
-#
-# Note: `delta` can also be negative, in which case the resulting delta will be a time
-# addition instead of a substraction, but this doesn't seem very intuitive nor useful.
-#
-# Examples:
-# Assuming the current time is 2024-09-04 02:59:53-07:00
-#
-# A delta between now and a day ago:
-#
-# $ ./time_delta.py 1d
-# 2024-09-03 02:59:53-07:00 - 2024-09-04 02:59:53-07:00
-#
-# Shifting 7 seconds into the future:
-#
-# $ ./time_delta.py 1d 7s
-# 2024-09-03 03:00:00-07:00 - 2024-09-04 03:00:00-07:00
-#
-# Shifting 3 seconds into the past:
-#
-# $ ./time_delta.py 1d -3s
-# 2024-09-03 02:59:50-07:00 - 2024-09-04 02:59:50-07:00
-#
-# A delta between now and a day from now:
-#
-# $ ./time_delta.py -1d
-# 2024-09-04 02:59:50-07:00 - 2024-09-05 02:59:50-07:00
+"""Utility script to print two (recent) timestamps with the given delta.
 
+Timestamps are printed in ISO format.
+
+Usage:  `./time_delta.py [-h] [-n] [-v] delta [shift]`
+Output: `<before> - <after>  [# delta]`
+
+Source: https://github.com/tellezhector/time_delta
+"""
+
+_ARGS="""\
+The fist timestamp is always the in the past in respect to the second (or be 
+the same).
+
+ delta  can be of the formats: 
+        1day,       1day3minutes,       1d3m,      3h,      1h, 3h2m50s 
+        (or equivalently)
+        1:00:00:00,   1:00:03:00, 1:00:03:00, 3:00:00, 1:00:00, 3:02:50
+
+ shift  optional. Can be of any of the same kind of formats as `delta`. 
+        If undefined, <after> will be 'now'; if defined, the dates will be 
+        shifted by the time specified, `shift` can be preceeded by a
+        minus (-) or plus (+) sign, a minus sign shifts "to the past", a plus
+        sign shifts "to the future", plus sign is the same as no sign.
+
+    -n  do not print a new line at the end.
+
+    -v  print the original delta after the timestamps in format:
+        <before> - <after>  #  delta
+        this is verifying the delta length easier visually.
+
+    -h  prints help, -hh and -hhh will print more details increasingly.
+
+Note: `delta` can also be negative, in which case the resulting delta will be 
+a time addition instead of a substraction, but this doesn't seem very 
+intuitive nor useful.
+"""
+
+_EXAMPLES="""\
+Examples:
+
+Assuming the current time is 2024-09-04 02:59:53-07:00
+
+A delta between now and a day ago:
+
+$ ./time_delta.py 1d
+2024-09-03 02:59:53-07:00 - 2024-09-04 02:59:53-07:00
+
+Shifting 7 seconds into the future:
+
+$ ./time_delta.py 1d 7s
+2024-09-03 03:00:00-07:00 - 2024-09-04 03:00:00-07:00
+
+Shifting 3 seconds into the past:
+
+$ ./time_delta.py 1d -3s
+2024-09-03 02:59:50-07:00 - 2024-09-04 02:59:50-07:00
+
+The same but including the delta:
+
+$ ./time_delta.py -v 1d -3s
+2024-09-03 02:59:50-07:00 - 2024-09-04 02:59:50-07:00  # 1d
+
+A delta between now and a day from now:
+
+$ ./time_delta.py -1d
+2024-09-04 02:59:50-07:00 - 2024-09-05 02:59:50-07:00
+"""
+
+import argparse
 import datetime
 import re
-import os
+import textwrap
 import sys
 
-DELTA_REGEX = re.compile(r'''^
+DELTA_REGEX = re.compile(
+    r'''^
         [-+]?
         ((?P<days>\d+?)(d|day|days))?
         ((?P<hours>\d+?)(h|hr|hour|hrs|hours))?
         ((?P<minutes>\d+?)(m|min|mins|minutes))?
         ((?P<seconds>\d+?)(s|sec|secs|seconds))?
-        $''', flags=re.VERBOSE)
+        $''',
+    flags=re.VERBOSE,
+)
 CLOCK_TIME_REGEX = re.compile(r'^[-+]?(?P<clock>(\d+:){0,3}\d+)$', flags=re.VERBOSE)
+
 
 class TimeDeltaError(Exception):
     def __init__(self, message: str, *args: object) -> None:
         super().__init__(*[message, args])
         self.message = message
+
 
 def clock_format_to_delta(time_str: str) -> datetime.timedelta:
     time_str = time_str.strip()
@@ -75,17 +105,18 @@ def clock_format_to_delta(time_str: str) -> datetime.timedelta:
         match len(parts):
             case 4:
                 # days
-                secs += 3600*24*int(parts.pop(0))
+                secs += 3600 * 24 * int(parts.pop(0))
             case 3:
                 # hours
-                secs += 3600*int(parts.pop(0))
+                secs += 3600 * int(parts.pop(0))
             case 2:
                 # minutes
-                secs += 60*int(parts.pop(0))
+                secs += 60 * int(parts.pop(0))
             case 1:
                 # seconds
                 secs += int(parts.pop(0))
     return sign * datetime.timedelta(seconds=secs)
+
 
 def parse_long_delta(time_str: str) -> datetime.timedelta:
     match = DELTA_REGEX.fullmatch(time_str)
@@ -97,6 +128,7 @@ def parse_long_delta(time_str: str) -> datetime.timedelta:
     delta = datetime.timedelta(**time_params)
     return sign * delta
 
+
 def parse_any_delta(time_str: str) -> datetime.timedelta:
     if DELTA_REGEX.fullmatch(time_str):
         return parse_long_delta(time_str)
@@ -104,7 +136,8 @@ def parse_any_delta(time_str: str) -> datetime.timedelta:
         return clock_format_to_delta(time_str)
     raise TimeDeltaError(f"bad time '{time_str}'")
 
-def main(args: list[str], print_end: str|None):
+
+def main(args: list[str], print_end: str, print_delta: bool):
     try:
         if len(args) < 1:
             raise TimeDeltaError('too few arguments')
@@ -115,25 +148,44 @@ def main(args: list[str], print_end: str|None):
             [delta_string], shift_string = args, None
         elif len(args) == 2:
             delta_string, shift_string = args
+
         delta = parse_any_delta(delta_string)
         now = datetime.datetime.now().astimezone()
+        shift = datetime.timedelta(seconds=0)
         if shift_string:
             shift = parse_any_delta(shift_string)
-            now = now + shift
+        now = now + shift
         then = now - delta
         if then > now:
             now, then = then, now
-        now_display = now.astimezone().isoformat(sep=' ',timespec='seconds')
-        then_display = then.astimezone().isoformat(sep=' ',timespec='seconds')
-        print(f'{then_display} - {now_display}', end=print_end)
+        now_display = now.astimezone().isoformat(sep=' ', timespec='seconds')
+        then_display = then.astimezone().isoformat(sep=' ', timespec='seconds')
+        res = f'{then_display} - {now_display}'
+        if print_delta:
+            res = f'{res}  # {delta_string}'
+        print(res, end=print_end)
     except TimeDeltaError as e:
         print(f'[{e.message}] args: {args}', file=sys.stderr, end=print_end)
         exit(1)
 
+
 if __name__ == '__main__':
-   args = sys.argv[1:]
-   print_end = None
-   if '-n' in args:
-       print_end = ''
-       args.remove('-n')
-   main(args, print_end)
+    parser = argparse.ArgumentParser('time_delta.py', add_help=False)
+    parser.add_argument('-n', dest='no_new_line', action='store_true')
+    parser.add_argument('-h', dest='help', action='count', default=0)
+    parser.add_argument('-v', dest='print_delta', action='store_true')
+
+
+    known_args, args = parser.parse_known_args()
+
+    if known_args.help:
+        if known_args.help >= 1:
+            print(textwrap.dedent(__doc__), file=sys.stderr)
+        if known_args.help >= 2:
+            print(textwrap.dedent(_ARGS), file=sys.stderr)
+        if known_args.help >= 3:
+            print(textwrap.dedent(_EXAMPLES), file=sys.stderr)
+        exit(0)
+
+    print_end='' if known_args.no_new_line else None
+    main(args, print_end=print_end, print_delta=known_args.print_delta)
